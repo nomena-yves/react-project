1e62cfde0f588f59a0ce33598c15595146c80225
nomena <hei.nomenjanahary@gmail.com>
2025-07-03 13:05:12 +0300

    premier commit

---
+++ votre-projet/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+return <div className="w-full bg-blue-950 border-2">
+  <div><img src="" alt="" /></div>
+  <div><a href="" className="text-white">Acceuil</a><a href=""  className="text-white">Actualité</a><a href=""  className="text-white">Bourse d'Etude</a><a href=""  className="text-white">Inscription</a><a href=""  className="text-white">Intrinet</a></div>
+</div>
+}
+
+export default App
