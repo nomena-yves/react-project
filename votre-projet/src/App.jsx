f6444f81f2a9d2849775a55c843e014b09c426c0
Rubis <hei.rubis.8@gmail.com>
2025-07-05 08:00:26 +0200

    changement du format

--- votre-projet/src/App.jsx
+++ votre-projet/src/App.jsx
@@ -1,8 +1,14 @@
 
 function App() {
-return <div className="w-full bg-blue-950 border-2 ">
-  <div><img src="" alt="" /></div>
-  <div className="border border-green-700 flex gap-4"><a href="" className="text-white">Acceuil</a><a href=""  className="text-white">Actualité</a><a href=""  className="text-white">Bourse d'Etude</a><a href=""  className="text-white">Inscription</a><a href=""  className="text-white">Intrinet</a></div>
+
+return <div className="w-full ">
+
+ <nav className="w-full border-y-indigo-900 ">
+ <div><img src="" alt="" /></div>
+  <div className="border border-green-700 flex gap-4"><a href="" className="text-white">Acceuil</a><a href="" 
+   className="text-white">Actualité</a><a href=""  className="text-white">Bourse d'Etude</a><a href="" 
+    className="text-white">Inscription</a><a href=""  className="text-white">Intrinet</a></div>
+ </nav>
 </div>
 }
 
