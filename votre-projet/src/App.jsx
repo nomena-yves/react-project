c0f1e75688f90b1cecb796f31a4b6829a1d0f9cf
Rubis <hei.rubis.8@gmail.com>
2025-07-05 08:21:27 +0200

    3eme changement

--- votre-projet/src/App.jsx
+++ votre-projet/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
 return <div className="w-full ">
 
- <nav className="w-full bg-blue-950 ">
+ <nav className="w-full border-y-indigo-900 ">
  <div><img src="" alt="" /></div>
   <div className="border border-green-700 flex gap-4"><a href="" className="text-white">Acceuil</a><a href="" 
    className="text-white">Actualité</a><a href=""  className="text-white">Bourse d'Etude</a><a href="" 
