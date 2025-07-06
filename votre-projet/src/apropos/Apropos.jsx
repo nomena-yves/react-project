792dc9e0cf5f4c237a229d448d1f4ac9eebf2c60
nomena <hei.nomenjanahary@gmail.com>
2025-07-06 17:14:28 +0300

    finalisation de lapartie final

--- votre-projet/src/apropos/Apropos.jsx
+++ votre-projet/src/apropos/Apropos.jsx
@@ -1,7 +1,7 @@
 function Apropos() {
     return <div className="bg-blue-950 px-10">
         <img className="w-20" src="/image/heiSchool.jpg" alt="" />
-        <div className="flex row-auto justify-around">
+        <div className="flex row-auto justify-between">
             <div className="w-50">
                 <p className="text-center text-white">Formation habilitée par l’Etat suivant le système LMD</p>
                 <br />
@@ -12,6 +12,17 @@ function Apropos() {
                 <p className="text-white">II J 161 R Ambodivoanjo </p>
                 <p className="text-white">Ivandry Antananarivo</p>
                 <p className="text-white">101, Madagascar</p>
+            </div>
+            <div className="flex flex-col">
+                <h1  className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-3xl lg:text-3xl dark:text-white text-center">Navigation</h1>
+                <button className="text-white">Accueil</button>
+                <button className="text-white">Actualite</button>
+                <button className="text-white">Bourse d'inscription</button>
+                <button className="text-white">Inscription</button>
+            </div>
+            <div>
+            <h1  className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-3xl lg:text-3xl dark:text-white text-center">Media sociaux</h1>
+
             </div>
         </div>
     </div>
